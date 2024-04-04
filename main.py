f4ebec153ad4e6238cbe58ae6eda61073728c8aa
AzlanSindhi <145383440+AzlanSindhi@users.noreply.github.com>
2024-04-04 22:49:59 +0530

    Update main.py
    adding comments for understanding

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ for x in range(0, 5):
 def print_board(board):
   for row in board:
     print (" ".join(row))
-
+#print the ocean in 5x5 matrix 
 print_board(board)
 
 def random_row(board):
@@ -17,31 +17,38 @@ def random_row(board):
 def random_col(board):
   return randint(0, len(board[0]) - 1)
 
+#ship_row and ship_col given random space in the ocean which player has to guess
 ship_row = random_row(board)
 ship_col = random_col(board)
-print (ship_row)
-print (ship_col)
+#this comments are for testing
+#print (ship_row)
+#print (ship_col)
 
-# Everything from here on should be in your for loop
-# don't forget to properly indent!
+
 for turn in range(8):
   print ("Turn", turn + 1)
   guess_row = int(input("Guess Row: "))
   guess_col = int(input("Guess Col: "))
 
   if guess_row == ship_row and guess_col == ship_col:
+    #if the guess is right
     print ("Congratulations! You sank my battleship!")
     break
   else:
+    #if the guess is out of range
     if guess_row not in range(5) or \
       guess_col not in range(5):
       print ("Oops, that's not even in the ocean.")
+    #if the guess is already made
     elif board[guess_row][guess_col] == "X":
       print ("You guessed that one already." )
     else:
+      #if the guess is wrong
       print ("You missed my battleship!")
       board[guess_row][guess_col] = "X"
     if (turn == 8):
+      #when turn counter reaches 8, the game is over
       print ("Game Over")
 
 print_board(board)
+#THE END
